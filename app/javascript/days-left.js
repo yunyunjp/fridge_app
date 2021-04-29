2ce4274578e978ea3a9ee226bb7d538eb23014b8
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-04-30 08:34:40 +0900

    賞味期限を過ぎると表示の色が変わる

--- app/javascript/days-left.js
+++ app/javascript/days-left.js
@@ -5,6 +5,11 @@ window.addEventListener('load', () => {
     const today = new Date();
     const expirationDate = new Date(expirationInput.innerHTML);
     const daysLeft = document.getElementById("days-left"+i);
-    daysLeft.innerHTML = (Math.ceil( (expirationDate.getTime() - today.getTime()) / ( 1000 * 60 * 60 * 24 )));
-  }  
+    daysLeft.innerHTML = (Math.round( (expirationDate.getTime() - today.getTime()) / ( 1000 * 60 * 60 * 24 )));
+    if (daysLeft.innerHTML <= 0) {
+     const deadLine = document.getElementById("dead-line"+i);
+     deadLine.setAttribute("style", "color:#7945F5;text-decoration:underline #7945F5;");
+     daysLeft.setAttribute("style", "color:#7945F5;text-decoration:underline #7945F5;");
+    };
+  };
 });
--- app/javascript/daysleft.js
+++ app/javascript/daysleft.js
@@ -4,4 +4,9 @@ window.addEventListener('load', () => {
   const expirationDate = new Date(expirationInput.innerHTML);
   const daysLeft = document.getElementById("days_left");
   daysLeft.innerHTML = (Math.ceil( (expirationDate.getTime() - today.getTime()) / ( 1000 * 60 * 60 * 24 )));
+ if (daysLeft.innerHTML <= 0) {
+     const deadLine = document.getElementById("dead_line");
+     deadLine.setAttribute("style", "color:#7945F5;text-decoration:underline #7945F5;");
+     daysLeft.setAttribute("style", "color:#7945F5;text-decoration:underline #7945F5;");
+    };
 });
