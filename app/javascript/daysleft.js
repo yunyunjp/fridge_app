2cb10e74c56c86178669c9847da7b9f8571071df
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-05-04 15:41:36 +0900

    ceilに再変更

--- app/javascript/daysleft.js
+++ app/javascript/daysleft.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
   const today = new Date();
   const expirationDate = new Date(expirationInput.innerHTML);
   const daysLeft = document.getElementById("days_left");
-  daysLeft.innerHTML = (Math.round( (expirationDate.getTime() - today.getTime()) / ( 1000 * 60 * 60 * 24 )));
+  daysLeft.innerHTML = (Math.ceil( (expirationDate.getTime() - today.getTime()) / ( 1000 * 60 * 60 * 24 )));
  if (daysLeft.innerHTML <= 0) {
      const deadLine = document.getElementById("dead_line");
      deadLine.setAttribute("style", "color:#7945F5;text-decoration:underline #7945F5;");
